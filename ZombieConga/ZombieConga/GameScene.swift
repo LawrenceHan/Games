55a691523e7591a63d8130f71b2e6445adfae1d6
LawrenceHan <yangfei6565@163.com>
2016-03-16 23:14:56 +0800

    Created project 1.

---
+++ ZombieConga/ZombieConga/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  ZombieConga
+//
+//  Created by Hanguang on 3/16/16.
+//  Copyright (c) 2016 Hanguang. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        backgroundColor = SKColor.blackColor()
+        
+        let background = SKSpriteNode(imageNamed: "background1")
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        background.anchorPoint = CGPoint(x: 0.5, y: 0.5) // default
+        background.zPosition = -1
+        //background.zRotation = CGFloat(M_PI) / 8
+        addChild(background)
+        
+        let mySize = background.size
+        print("Size: \(mySize)")
+    }
+}
---
+++ ZombieConga/ZombieConga/GameViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GameViewController.swift
+//  ZombieConga
+//
+//  Created by Hanguang on 3/16/16.
+//  Copyright (c) 2016 Hanguang. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size:CGSize(width: 2048, height: 1536))
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .AspectFill
+        skView.presentScene(scene)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
