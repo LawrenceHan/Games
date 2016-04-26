484f3e3e8996a5f1983c2ce7bcf938aaa6eb49a5
LawrenceHan <yangfei6565@163.com>
2016-04-27 00:02:39 +0800

    Added Chapter 9 code.

---
+++ SpriteKitPhysicsTest.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import SpriteKit
+import XCPlayground
+
+let sceneView = SKView(frame: CGRect(x: 0, y: 0, width: 480, height: 320))
+let scene = SKScene(size: CGSize(width: 480, height: 320))
+scene.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+sceneView.showsFPS = true
+sceneView.presentScene(scene)
+
+XCPlaygroundPage.currentPage.liveView = sceneView
+
+let square = SKSpriteNode(imageNamed: "square")
+square.name = "shape"
+square.position = CGPoint(x: scene.size.width * 0.25,
+                          y: scene.size.height * 0.50)
+
+let circle = SKSpriteNode(imageNamed: "circle")
+circle.name = "shape"
+circle.position = CGPoint(x: scene.size.width * 0.50,
+                          y: scene.size.height * 0.50)
+
+let triangle = SKSpriteNode(imageNamed: "triangle")
+triangle.name = "shape"
+triangle.position = CGPoint(x: scene.size.width * 0.75,
+                            y: scene.size.height * 0.50)
+
+scene.addChild(square)
+scene.addChild(circle)
+scene.addChild(triangle)
+
+circle.physicsBody = SKPhysicsBody(circleOfRadius: circle.size.width/2)
+
